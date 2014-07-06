8d6630569b127779ca807e4bb6301161af54d24d
Ivan Wang <icw@cmu.edu>
2014-07-05 18:40:40 -0700

    Dynamically resize terminal

--- static/js/app.js
+++ static/js/app.js
@@ -23,17 +23,30 @@
         }
 
         if (terminal = $.terminal.active()) {
-            args = _.isString(args) ? args : args.toString();
+            arguments = _.map(arguments, function(arg) {
+                return _.isString(arg) ? arg : arg.toString();
+            });
             var printTerminal = error ? terminal.error : terminal.echo;
-            printTerminal(args);
+            printTerminal(arguments.join(' '));
         }
     };
 
-    /* Terminals */
+    /* Terminal objects */
     var terminal = new Terminal(system);
+    var $term = $('#terminal');
 
     $(document).ready(function() {
-        $('#terminal').terminal({}, terminal.options.base);
+        $term.terminal({}, terminal.options.base);
+
+        var resizeTerminal = function() {
+            var newHeight = Math.round($(window).height()) - 120;
+            $term.height(_.max([100, newHeight]));
+            $term.resize();
+        };
+        resizeTerminal(); // Set initial size.
+
+        // Resize terminal on window resize.
+        $(window).resize(_.debounce(resizeTerminal, 150));
     });
 
 })();
